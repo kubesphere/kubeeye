77d05438586302fc90d8f8c280eceb9477834031
wenjuan <sophiezhao@yunify.com>
2022-06-09 11:32:15 +0800

    add audit percent

--- pkg/audit/audit.go
+++ pkg/audit/audit.go
@@ -15,83 +15,132 @@
 package audit
 
 import (
-    "context"
-    
-    "github.com/kubesphere/kubeeye/apis/kubeeye/v1alpha1"
-    "github.com/kubesphere/kubeeye/pkg/kube"
-    "github.com/kubesphere/kubeeye/pkg/regorules"
-    "github.com/pkg/errors"
-    "sigs.k8s.io/controller-runtime/pkg/log"
+	"context"
+	"sync"
+
+	"github.com/kubesphere/kubeeye/apis/kubeeye/v1alpha1"
+	"github.com/kubesphere/kubeeye/pkg/kube"
+	"github.com/kubesphere/kubeeye/pkg/regorules"
+	"github.com/pkg/errors"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var (
-    workloads = "data.kubeeye_workloads_rego"
-    rbac      = "data.kubeeye_RBAC_rego"
-    nodes     = "data.kubeeye_nodes_rego"
-    events    = "data.kubeeye_events_rego"
-    certexp   = "data.kubeeye_certexpiration"
+	workloads = "data.kubeeye_workloads_rego"
+	rbac      = "data.kubeeye_RBAC_rego"
+	nodes     = "data.kubeeye_nodes_rego"
+	events    = "data.kubeeye_events_rego"
+	certexp   = "data.kubeeye_certexpiration"
 )
 
-func Cluster(ctx context.Context, kubeConfigPath string, additionalregoruleputh string, output string) error {
-    kubeConfig, err := kube.GetKubeConfig(kubeConfigPath)
-    if err != nil {
-        return errors.Wrap(err, "Failed to load config file")
-    }
-    
-    var kc kube.KubernetesClient
-    clients, err := kc.K8SClients(kubeConfig)
-    if err != nil {
-        return err
-    }
-    
-    _, validationResultsChan := ValidationResults(ctx, clients, additionalregoruleputh)
-    
-    // Set the output mode, support default output JSON and CSV.
-    switch output {
-    case "JSON", "json", "Json":
-        if err := JSONOutput(validationResultsChan); err != nil {
-            return err
-        }
-    case "CSV", "csv", "Csv":
-        if err := CSVOutput(validationResultsChan); err != nil {
-            return err
-        }
-    default:
-        if err := defaultOutput(validationResultsChan); err != nil {
-            return err
-        }
-    }
-    return nil
+var AuditPercent = sync.Map{}
+
+type PercentOutput struct {
+	TotalAuditCount   int
+	CurrentAuditCount int
+	AuditPercent      int
 }
 
-func ValidationResults(ctx context.Context, kubernetesClient *kube.KubernetesClient, additionalregoruleputh string) (kube.K8SResource, <-chan []v1alpha1.AuditResults) {
-    logs := log.FromContext(ctx)
-    
-    // get kubernetes resources and put into the channel.
-    logs.Info("starting get kubernetes resources")
-    go func(ctx context.Context, kubernetesClient *kube.KubernetesClient) {
-        err := kube.GetK8SResourcesProvider(ctx, kubernetesClient)
-        if err != nil {
-            logs.Error(err, "failed to get kubernetes resources")
-        }
-    }(ctx, kubernetesClient)
-    
-    k8sResources := <-kube.K8sResourcesChan
-    
-    logs.Info("getting and merging the Rego rules")
-    regoRulesChan := regorules.MergeRegoRules(ctx, regorules.GetDefaultRegofile("rules"), regorules.GetAdditionalRegoRulesfiles(additionalregoruleputh))
-    
-    logs.Info("starting audit kubernetes resources")
-    RegoRulesValidateChan := MergeRegoRulesValidate(ctx, regoRulesChan,
-        RegoRulesValidate(workloads, k8sResources),
-        RegoRulesValidate(rbac, k8sResources),
-        RegoRulesValidate(events, k8sResources),
-        RegoRulesValidate(nodes, k8sResources),
-        RegoRulesValidate(certexp, k8sResources),
-    )
-    
-    // ValidateResources Validate Kubernetes Resource, put the results into the channels.
-    logs.Info("get audit results")
-    
-    return k8sResources, RegoRulesValidateChan
+func Cluster(ctx context.Context, kubeConfigPath string, additionalregoruleputh string, output string) error {
+	kubeConfig, err := kube.GetKubeConfig(kubeConfigPath)
+	if err != nil {
+		return errors.Wrap(err, "Failed to load config file")
+	}
+
+	var kc kube.KubernetesClient
+	clients, err := kc.K8SClients(kubeConfig)
+	if err != nil {
+		return err
+	}
+
+	_, validationResultsChan := ValidationResults(ctx, clients, additionalregoruleputh, "")
+
+	// Set the output mode, support default output JSON and CSV.
+	switch output {
+	case "JSON", "json", "Json":
+		if err := JSONOutput(validationResultsChan); err != nil {
+			return err
+		}
+	case "CSV", "csv", "Csv":
+		if err := CSVOutput(validationResultsChan); err != nil {
+			return err
+		}
+	default:
+		if err := defaultOutput(validationResultsChan); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//将名字传进去
+func ValidationResults(ctx context.Context, kubernetesClient *kube.KubernetesClient, additionalregoruleputh string, clusterInsigntName string) (kube.K8SResource, <-chan []v1alpha1.AuditResults) {
+	logs := log.FromContext(ctx)
+
+	// get kubernetes resources and put into the channel.
+	logs.Info("starting get kubernetes resources")
+	go func(ctx context.Context, kubernetesClient *kube.KubernetesClient) {
+		err := kube.GetK8SResourcesProvider(ctx, kubernetesClient)
+		if err != nil {
+			logs.Error(err, "failed to get kubernetes resources")
+		}
+	}(ctx, kubernetesClient)
+
+	k8sResources := <-kube.K8sResourcesChan
+
+	percent, ok := AuditPercent.Load(clusterInsigntName)
+	var auditPercent *PercentOutput
+	if !ok {
+		auditPercent = &PercentOutput{}
+		AuditPercent.Store(clusterInsigntName, auditPercent)
+	} else {
+		auditPercent = percent.(*PercentOutput)
+	}
+
+	if k8sResources.Deployments != nil {
+		auditPercent.TotalAuditCount += len(k8sResources.Deployments.Items)
+	}
+	if k8sResources.StatefulSets != nil {
+		auditPercent.TotalAuditCount += len(k8sResources.StatefulSets.Items)
+	}
+	if k8sResources.DaemonSets != nil {
+		auditPercent.TotalAuditCount += len(k8sResources.DaemonSets.Items)
+	}
+	if k8sResources.Jobs != nil {
+		auditPercent.TotalAuditCount += len(k8sResources.Jobs.Items)
+	}
+	if k8sResources.CronJobs != nil {
+		auditPercent.TotalAuditCount += len(k8sResources.CronJobs.Items)
+	}
+	if k8sResources.Roles != nil {
+		auditPercent.TotalAuditCount += len(k8sResources.Roles.Items)
+	}
+	if k8sResources.ClusterRoles != nil {
+		auditPercent.TotalAuditCount += len(k8sResources.ClusterRoles.Items)
+	}
+	if k8sResources.Nodes != nil {
+		auditPercent.TotalAuditCount += len(k8sResources.Nodes.Items)
+	}
+	if k8sResources.Events != nil {
+		auditPercent.TotalAuditCount += len(k8sResources.Events.Items)
+	}
+	auditPercent.TotalAuditCount++
+	auditPercent.CurrentAuditCount = auditPercent.TotalAuditCount
+
+	logs.Info("getting and merging the Rego rules")
+	regoRulesChan := regorules.MergeRegoRules(ctx, regorules.GetDefaultRegofile("rules"), regorules.GetAdditionalRegoRulesfiles(additionalregoruleputh))
+
+	logs.Info("starting audit kubernetes resources")
+	RegoRulesValidateChan := MergeRegoRulesValidate(ctx, regoRulesChan,
+		RegoRulesValidate(workloads, k8sResources, auditPercent),
+		RegoRulesValidate(rbac, k8sResources, auditPercent),
+		RegoRulesValidate(events, k8sResources, auditPercent),
+		RegoRulesValidate(nodes, k8sResources, auditPercent),
+		RegoRulesValidate(certexp, k8sResources, auditPercent),
+	)
+
+	// ValidateResources Validate Kubernetes Resource, put the results into the channels.
+	logs.Info("get audit results")
+
+	return k8sResources, RegoRulesValidateChan
 }
