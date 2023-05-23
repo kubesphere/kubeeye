e1193c709d9262234f21a27add0970994f488f92
Xiao Liu <46879761+liangzai006@users.noreply.github.com>
2023-05-23 10:10:06 +0800

    Add result get

---
+++ apis/kubeeye/v1alpha2/inspectresult_types.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// InspectResultSpec defines the desired state of InspectResult
+type InspectResultSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	FileChangeResult []FileChangeResult `json:"fileChangeResult,omitempty"`
+}
+
+// InspectResultStatus defines the observed state of InspectResult
+type InspectResultStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+type FileChangeResult struct {
+	NodeName string                 `json:"nodeName,omitempty"`
+	Info     []FileChangeResultItem `json:"info,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// InspectResult is the Schema for the inspectresults API
+type InspectResult struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   InspectResultSpec   `json:"spec,omitempty"`
+	Status InspectResultStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// InspectResultList contains a list of InspectResult
+type InspectResultList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []InspectResult `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&InspectResult{}, &InspectResultList{})
+}
---
+++ examples/test/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	kubeeyev1alpha2 "github.com/kubesphere/kubeeye/apis/kubeeye/v1alpha2"
+)
+
+func main() {
+
+	//cluster, _ := kube.GetKubeConfigInCluster()
+	//var kc kube.KubernetesClient
+	//clients, _ := kc.K8SClients(cluster)
+
+	//list, err := clients.ClientSet.CoreV1().ServiceAccounts("kubeeye-system").List(context.Background(), metav1.ListOptions{})
+	//klog.Info(list)
+	//var resetNum int32 = 5
+	//var Parallelism int32 = 3
+	//job := v1.Job{
+	//	ObjectMeta: metav1.ObjectMeta{
+	//		Name: "test",
+	//	},
+	//	Spec: v1.JobSpec{
+	//		BackoffLimit: &resetNum,
+	//		Completions:  &resetNum,
+	//		Parallelism:  &Parallelism,
+	//		Template: corev1.PodTemplateSpec{
+	//			ObjectMeta: metav1.ObjectMeta{Name: "test-pod"},
+	//			Spec: corev1.PodSpec{
+	//				Containers: []corev1.Container{{
+	//					Name:    "inspect-jobs",
+	//					Image:   "jw008/kubeeye:test",
+	//					Command: []string{"inspect"},
+	//					Args:    []string{"file-change", "--task-name", "inspect"},
+	//					VolumeMounts: []corev1.VolumeMount{{
+	//						Name:      "root-path",
+	//						ReadOnly:  true,
+	//						MountPath: "/host",
+	//					}},
+	//					ImagePullPolicy: "Always",
+	//				}},
+	//				ServiceAccountName: "",
+	//				RestartPolicy:      corev1.RestartPolicyNever,
+	//				Volumes: []corev1.Volume{{
+	//					Name: "root-path",
+	//					VolumeSource: corev1.VolumeSource{
+	//						HostPath: &corev1.HostPathVolumeSource{
+	//							Path: "/",
+	//						},
+	//					},
+	//				}},
+	//			},
+	//		},
+	//	},
+	//}
+	//create, err := clients.ClientSet.BatchV1().Jobs("kubeeye-system").Create(context.Background(), &job, metav1.CreateOptions{})
+	//
+	//if err != nil {
+	//	klog.Error(err)
+	//}
+	//klog.Info(create)
+
+	//result := &kubeeyev1alpha2.InspectResult{
+	//	ObjectMeta: metav1.ObjectMeta{Name: "test-result"},
+	//	Spec:       kubeeyev1alpha2.InspectResultSpec{Name: "fileChange"},
+	//}
+	//i, _ := json.Marshal([]string{"master", "node1", "node2", "node3"})
+	//s := map[string][]byte{"node1": i}
+	//
+	//get, err := clients.VersionClientSet.KubeeyeV1alpha2().InspectResults("kubeeye-system").Get(context.TODO(), result.Name, metav1.GetOptions{})
+	//if err != nil {
+	//	klog.Error(err)
+	//	if kubeErr.IsNotFound(err) {
+	//		marshal, err := json.Marshal(s)
+	//		klog.Error(err)
+	//
+	//		ext := runtime.RawExtension{}
+	//		ext.Raw = marshal
+	//		result.Spec.Result = ext
+	//
+	//		create, err := clients.VersionClientSet.KubeeyeV1alpha2().InspectResults("kubeeye-system").Create(context.TODO(), result, metav1.CreateOptions{})
+	//		klog.Info(create, err)
+	//		os.Exit(1)
+	//	}
+	//	os.Exit(1)
+	//}
+	//
+	//klog.Info(get.Spec.Result)
+	//var data = make(map[string][]byte)
+	//err = json.Unmarshal(get.Spec.Result.Raw, &data)
+	//if err != nil {
+	//	klog.Error(err)
+	//}
+	//fmt.Println(rand.Intn(100))
+	//data[fmt.Sprintf("node%d", rand.Intn(100))] = s["node1"]
+	//marshal, err := json.Marshal(data)
+	//get.Spec.Result.Raw = marshal
+	//_, err = clients.VersionClientSet.KubeeyeV1alpha2().InspectResults("kubeeye-system").Update(context.TODO(), get, metav1.UpdateOptions{})
+	//klog.Error(err)
+	fmt.Println(IsComplete(nil))
+}
+func IsComplete(jobs []kubeeyev1alpha2.JobPhase) (status bool) {
+
+	return status
+
+}
